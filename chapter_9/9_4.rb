6b1de7213f34b0a20d9bd4ef57d2030ab450ad2e
LNA <LaToyaNAllen@gmail.com>
2014-02-18 16:02:09 -0600

    deleted files with old names

--- chapter_9/9_4.rb
+++
@@ -1,7 +0,0 @@
-describe 'EnvironmentController' do 
-  it "turns on the lo temp alarm at threashold" do 
-    way_too_cold
-
-    hw.get_state.should == "HBchL" # can quickly interperate the results
-  end
-end
